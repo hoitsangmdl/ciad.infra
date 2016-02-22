5ec67acbe43f377d5bcafa9824454b0f94246a04
Hoi Tsang <hoitsangmdl@gmail.com>
2016-02-21 22:16:38 -0500

    remove env and hoia

--- env/bin/tls.py
+++
@@ -1,4 +0,0 @@
-#!/Users/hoitsang/projects/githuben/ciad.infra/env/bin/python
-# EASY-INSTALL-SCRIPT: 'tlslite==0.4.9','tls.py'
-__requires__ = 'tlslite==0.4.9'
-__import__('pkg_resources').run_script('tlslite==0.4.9', 'tls.py')
